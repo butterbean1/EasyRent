b05683f443f87a4914ce6a6733a267d8875acc4f
butterbean1 <butterbean@mail.ru>
2021-05-26 14:51:40 +0300

    fragment rooms list added

--- app/src/main/java/ru/butterbean/easyrent/screens/SplashFragment.kt
+++ app/src/main/java/ru/butterbean/easyrent/screens/SplashFragment.kt
@@ -1,10 +1,13 @@
 package ru.butterbean.easyrent.screens
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.butterbean.easyrent.R
 
 class SplashFragment : Fragment() {
@@ -14,6 +17,12 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_roomsListFragment)
+        },3000)
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
