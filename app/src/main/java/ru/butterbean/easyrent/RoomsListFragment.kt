daabc30b14ed24f5bea59b43d46de65095ba706c
butterbean <butterbean@mail.ru>
2021-05-27 21:43:50 +0300

    actions added bugs fixed

--- app/src/main/java/ru/butterbean/easyrent/RoomsListFragment.kt
+++ app/src/main/java/ru/butterbean/easyrent/RoomsListFragment.kt
@@ -1,11 +1,10 @@
 package ru.butterbean.easyrent
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_rooms_list.view.*
