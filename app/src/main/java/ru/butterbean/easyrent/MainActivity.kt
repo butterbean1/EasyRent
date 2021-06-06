5e4f48dff6da353127ad1482533d1d907e94ecbe
butterbean <butterbean@mail.ru>
2021-06-06 15:36:36 +0300

    nav added

--- app/src/main/java/ru/butterbean/easyrent/MainActivity.kt
+++ app/src/main/java/ru/butterbean/easyrent/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.butterbean.easyrent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import ru.butterbean.easyrent.database.MainDatabase
 import ru.butterbean.easyrent.databinding.ActivityMainBinding
 import ru.butterbean.easyrent.screens.SplashFragment
@@ -10,12 +11,14 @@ import ru.butterbean.easyrent.utils.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mBinding:ActivityMainBinding
+    private var _binding:ActivityMainBinding? = null
+    val mBinding get() = _binding!!
     lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
         APP_ACTIVITY = this
@@ -32,4 +35,8 @@ class MainActivity : AppCompatActivity() {
         replaceFragment(SplashFragment(),false)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
