d94abd98fee6eac28c44f555c73e2ab048fd5691
butterbean1 <butterbean@mail.ru>
2021-06-15 11:28:59 +0300

    in process

--- app/src/main/java/ru/butterbean/easyrent/database/dastabaseMigrations.kt
+++ app/src/main/java/ru/butterbean/easyrent/database/dastabaseMigrations.kt
@@ -5,19 +5,21 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("CREATE TABLE $TABLE_RESERVES_ARCHIVE_NAME(" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                "roomId INTEGER NOT NULL, " +
-                "guestName TEXT NOT NULL, " +
-                "guestsCount INTEGER NOT NULL, " +
-                "sum INTEGER NOT NULL, " +
-                "payment INTEGER NOT NULL, " +
-                "dateCheckIn TEXT NOT NULL, " +
-                "dateCheckOut TEXT NOT NULL, " +
-                "wasCheckIn INTEGER NOT NULL, " +
-                "wasCheckOut INTEGER NOT NULL," +
-                "FOREIGN KEY(roomId) REFERENCES rooms_table(id) ON UPDATE NO ACTION ON DELETE CASCADE" +
-                ");" +
-                "CREATE INDEX index_${TABLE_RESERVES_ARCHIVE_NAME}_roomId ON $TABLE_RESERVES_ARCHIVE_NAME (roomId)")
+        database.execSQL(
+            "CREATE TABLE $TABLE_RESERVES_ARCHIVE_NAME(" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "roomId INTEGER NOT NULL, " +
+                    "guestName TEXT NOT NULL, " +
+                    "guestsCount INTEGER NOT NULL, " +
+                    "sum INTEGER NOT NULL, " +
+                    "payment INTEGER NOT NULL, " +
+                    "dateCheckIn TEXT NOT NULL, " +
+                    "dateCheckOut TEXT NOT NULL, " +
+                    "wasCheckIn INTEGER NOT NULL, " +
+                    "wasCheckOut INTEGER NOT NULL," +
+                    "FOREIGN KEY(roomId) REFERENCES rooms_table(id) ON UPDATE NO ACTION ON DELETE CASCADE" +
+                    ") "
+        )
+        database.execSQL("CREATE INDEX index_${TABLE_RESERVES_ARCHIVE_NAME}_roomId ON $TABLE_RESERVES_ARCHIVE_NAME (roomId)")
     }
 }
